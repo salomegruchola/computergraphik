01094b0a7692beb663ebb523fda1c2cf6bf74a27
Uriel Brawer <uriel.brawer@students.unibe.ch>
2019-07-31 14:03:10 +0200

    ddd

--- assignment1/src/Cylinder.cpp
+++ assignment1/src/Cylinder.cpp
@@ -45,32 +45,54 @@ intersect(const Ray&  _ray,
     const vec3 &h = axis;
     //const vec3  nor = _normal;
     
+
+    
     // do ray tracing with an infinite cylinder
     // then determine whether the ray goes through in-between the two planes
     
     std::array<double, 2> t;
-    size_t nsol = solveQuadratic(dot(d,d) - dot(d,h)*dot(h,d) - dot(d,h)*dot(d,h) + dot(d,h)*dot(d,h) *                         dot(h,h),
-                                 dot(z,d)*dot(d,z) - dot(z,h) * dot(h,d) - dot(d,h) * dot(h,z) - dot(d,h) * dot(z,h) - dot(z,h) * dot(d,h) + dot(d,h) * dot(z,h) * dot(h,h) + dot(z,h) * dot(d,h) * dot(h,h)  ,
-                                dot(z,z) - 2 * dot(z,h)*dot(z,h) + dot(z,h)*dot(z,h) * dot(h,h) - radius*radius,t)   ;
-                                 
+    
+    double a;
+    double b;
+    
+   // a = dot(z,z) + 2*t* dot(d,z) + t*t * dot(d,d);
+   // b = dot(z,axis)*dot(z,axis) + 2*t * dot(z,axis) * dot(d,axis) + t * t * dot(d,axis) * dot(d,axis);
+    
+    double k,l,m;
+    k = dot(d,d) - dot(d,axis) * dot(d,axis);
+    l = 2 * dot(d,z) - 2 * dot(z,axis) * dot(d,axis);
+    m =dot(z,z) - dot(z,axis)*dot(z,axis) - radius * radius;
+    
+    size_t nsol = solveQuadratic(k,l,m ,t);
+    
+    
+  
     _intersection_t = NO_INTERSECTION;
+    
     
     // Find the closest valid solution (in front of the viewer)
     for (size_t i = 0; i < nsol; ++i) {
-        if (-t[i] > 0) _intersection_t = std::min(_intersection_t,t[i]);
+        if (t[i] > 0) _intersection_t = std::min(_intersection_t,t[i]);
         //printf("%f", t[i]);
     }
+    
 
     
     if (_intersection_t == NO_INTERSECTION) return false;
 
     _intersection_point  = _ray(_intersection_t);
     
-    if (abs(dot((_intersection_point - center),axis)) > 0.5 ) return false;
+    
+    
+    double height_ofIntersectionPoint = dot(_intersection_point - center,axis);
+    
+    if (height_ofIntersectionPoint * height_ofIntersectionPoint > 1  ) return false;
+    printf("%f", height_ofIntersectionPoint);
     _intersection_normal = normalize( (_intersection_point - center) -
                                      dot(_intersection_point - center,axis)*axis);
     
     return true;
+    
 }
 // make
 // ./raytrace ../scenes/cylinders/cylinders.sce cylinders_output.tga
